b00c96bfc339b42d795a0ec936d2b2c23cbaa69f
Robert Aubé <robert.aube@cegeplimoilou.ca>
2021-11-16 21:18:10 -0500

    Mise à jour suite à mon absence - G1 seulement (PAssager Et PassagerTest)

--- 201G1/prjAvion/src/Passager.java
+++ 201G1/prjAvion/src/Passager.java
@@ -1,22 +1,27 @@
 public class Passager {
-    public static final int AGE_DEFAUT = 50;
     public static final int POIDS_DEFAUT = 300;
-    public static final int AGE_MIN = 0;
-    public static final int POIDS_MIN = 0;
+    public static final int AGE_DEFAUT = 50;
 
-    //variable d'instance ou attribut
-    private int age;
     private int poids;
+    private int age;
 
-    //constructeur par défaut
+    //Constructeur par défaut
     public Passager() {
-        this(AGE_DEFAUT,POIDS_DEFAUT);
-//        this.age = AGE_DEFAUT;
-//        this.poids = POIDS_DEFAUT;
+        this(POIDS_DEFAUT, AGE_DEFAUT);
     }
 
-    public Passager(int age, int poids) {
-        this.age = age;
+    public Passager(int poids, int age) {
+        setPoids(poids);
+        setAge(age);
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+
+    public void setPoids(int poids) {
+        if (!poidsEstValide(poids))
+            throw new IllegalArgumentException("Un poids négatif n'est pas valide.");
         this.poids = poids;
     }
 
@@ -26,32 +31,60 @@ public class Passager {
 
     public void setAge(int age) {
         if (!ageEstValide(age))
-            throw new IllegalArgumentException("Age invalide : " + age);
+            throw new IllegalArgumentException("Un age négatif n'est pas valide.");
         this.age = age;
     }
 
-    public int getPoids() {
-        return poids;
-    }
-
-    public void setPoids(int poids) {
-        //A terminer!!!!
-        this.poids = poids;
+    public static boolean poidsEstValide(int poids) {
+        return 0 <= poids;
     }
 
     public static boolean ageEstValide(int age) {
-        boolean estValide;
-
-        estValide = age >= AGE_MIN;
-
-        return  estValide;
+        return 0 <= age;
     }
 
-    public static boolean poidsEstValide(int poids) {
-        boolean estValide;
-
-        estValide = poids >= POIDS_MIN;
-
-        return  estValide;
+    @Override
+    public String toString() {
+        return "Passager{" +
+                "poids=" + poids +
+                ", age=" + age +
+                '}';
     }
+
+    ///////////////////////////// PAS MONTRER ÇA POUR LE MOMENT//////////////////////
+    /**
+//     * Retourne Vrai si le passager reçu en argument est plus pesant que l'objet courant.
+//     * @param passager
+//     * @return Vrai si le passager reçu en argument est plus pesant que l'objet courant
+//     */
+//    public boolean estPlusPesant(Passager passager) {
+//        boolean estPlusLourd;
+//        estPlusLourd = this.poids < passager.getPoids();
+//        return estPlusLourd;
+//    }
+//
+//    /**
+//     * Retourne Vrai si le passager reçu en argument est plus vieux que l'objet courant.
+//     * @param passager
+//     * @return Vrai si le passager reçu en argument est plus vieux que l'objet courant
+//     */
+//    public boolean estPlusVieux(Passager passager) {
+//        boolean estPlusVieux;
+//        estPlusVieux = this.age < passager.getAge();
+//        return estPlusVieux;
+//    }
+//
+//    /**
+//     * Retourne Vrai si le passager1 est plus vieux que le passager2
+//     * @param passager1
+//     * @param passager2
+//     * @return Vrai si le passager1 est plus vieux que le passager2
+//     */
+//    public static boolean estPlusVieux(Passager passager1, Passager passager2) {
+//        boolean estPlusVieux;
+//        estPlusVieux = passager1.estPlusVieux(passager2);
+//        return estPlusVieux;
+//    }
+
+
 }
